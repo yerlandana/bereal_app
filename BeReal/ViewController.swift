a4c7ba90db5ffd569316e82270f06e97181759ba
Dana <dyerlanova@gmail.com>
2023-03-26 21:36:06 -0500

    first_commit

---
+++ BeReal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeReal
+//
+//  Created by Dana Yerlan on 3/26/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
